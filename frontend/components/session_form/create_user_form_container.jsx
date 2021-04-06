50941ed0fcb759c5623ed05fc8573238e3f6fcdf
Chris Hanma <hongchris96@hotmail.com>
2021-04-05 23:20:26 -0700

    Solved signup and login rendering bug, Add more css to those components

--- frontend/components/session_form/create_user_form_container.jsx
+++ frontend/components/session_form/create_user_form_container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { createUser } from '../../actions/session/session_actions';
-import SignUpForm from './login_form';
+import SignUpForm from './create_user_form';
 
 const mapSTP = state => ({
   formType: "Sign Up",
