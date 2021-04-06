8241f882de10403eaa8d00e5588b4b9d97a29421
Chris Hanma <hongchris96@hotmail.com>
2021-04-05 18:11:31 -0700

    Begin on frontend AuthRoute

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import {Route, Switch} from 'react-router-dom';
 
 // import list of containers
@@ -7,7 +8,10 @@ const App = () => {
   return(
     <div>
       <h1>disGuy app.jsx</h1>
-      {/* Routes goes here */}
+      <Switch>
+        <AuthRoute exact path="/login" component={LogInFormContainer} />
+        <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+      </Switch>
     </div>
   );
 };
