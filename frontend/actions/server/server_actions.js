43fe560a1d13a5017da483aa6abb93534e2f0775
Chris Hanma <hongchris96@hotmail.com>
2021-04-07 15:17:22 -0700

    Write server utils and actions, but no error handling

---
+++ frontend/actions/server/server_actions.js
@@ -0,0 +1,44 @@
+import * as ServerAPIUtil from '../../utils/server_api_util';
+
+export const RECEIVE_SERVERS = "RECEIVE_SERVERS";
+export const RECEIVE_SERVER = "RECEIVE_SERVER";
+export const REMOVE_SERVER = "RECEIVE_SERVER";
+
+export const RECEIVE_SERVER_ERRORS = "RECEIVE_SERVER_ERRORS";
+
+const receiveServers = servers => ({
+  type: RECEIVE_SERVERS,
+  servers
+});
+
+const receiveServer = server => ({
+  type: RECEIVE_SERVER,
+  server
+});
+
+const removeServer = serverId => ({
+  type: REMOVE_SERVER,
+  serverId
+});
+
+// receiveServerErrors ?
+
+export const requestServers = () => dispatch => {
+  return ServerAPIUtil.fetchServers().then(servers => dispatch(receiveServers(servers)));
+};
+
+export const requestServer = (serverId) => dispatch => {
+  return ServerAPIUtil.fetchServer(serverId).then(server => dispatch(receiveServer(server)));
+};
+
+export const createServer = (server) => dispatch => {
+  return ServerAPIUtil.createServer(server).then(server => dispatch(receiveServer(server)));
+};
+
+export const updateServer = (server) => dispatch => {
+  return ServerAPIUtil.updateServer(server).then(server => dispatch(receiveServer(server)));
+};
+
+export const deleteServer = (serverId) => dispatch => {
+  return ServerAPIUtil.deleteServer(serverId).then(() => dispatch(removeServer(serverId)));
+};
