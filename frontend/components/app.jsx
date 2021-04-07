30e243414e072da04734b5b2eed1e48e3826c46e
Chris Hanma <hongchris96@hotmail.com>
2021-04-06 21:21:46 -0700

    Complete home page and css of home page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,8 @@ import { AuthRoute, ProtectedRoute } from '../utils/route_util';
 import {Route, Switch} from 'react-router-dom';
 // import disguyText from '../../app/assets/images/disguy_logo.png';
 
+import Home from './home/home';
+
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/create_user_form_container';
 import PlaceholderContainer from './placeholder/placeholder_container';
@@ -13,6 +15,7 @@ const App = () => {
   return(
     <div>
       <Switch>
+        <Route exact path="/" component={Home} />
         <ProtectedRoute exact path="/placeholder" component={PlaceholderContainer} />
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <div className="home-div-1">
+        <nav>
+          <div>
+            <h3>DISGUY LOGO</h3>
+          </div>
+          <Link to="/login">Login</Link>
+        </nav>
+
+        <div className="home-body-1">
+          <h1>Your place to talk</h1>
+          <p>Whether you’re part of a school club, gaming group, worldwide art community, or just a 
+            handful of friends that want to spend time together, disGuy makes it easy to talk every day 
+            and hang out more often.</p>
+          <div className="home-body-1-buttons">
+            <a href="https://github.com/hongchris96/disGuy">Click for Github</a>
+            <Link to="/login">Open disGuy in your browser</Link>
+          </div>
+        </div>
+      </div>
+      
+      <div className="home-div-2">
+        <div>
+          <h1>{"¯\\_(ツ)_/¯"}</h1>
+        </div>
+        <div>
+          <h2>Some Title Here</h2>
+          <p>Some Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
