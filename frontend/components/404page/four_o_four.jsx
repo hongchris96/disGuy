b2104ce25dac1c735d7c20db3694d6f86688c849
Chris Hanma <hongchris96@hotmail.com>
2021-04-07 09:50:00 -0700

    Add 404 page

---
+++ frontend/components/404page/four_o_four.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Page404 = (props) => {
+  return (
+    <div className="page404">
+      <div className="body-404">
+        <div className="left-404">
+          <img src={window.title404URL}/>
+          <h2>WIZARDS BEHIND CURTAINS</h2>
+          <p>That is so 1939. disGuy is secretly powered by quantum robot bugeaters. 
+            Anyway, you look lost here. No match found for <code>{props.location.pathname}</code>. 
+            Here's the link back to Home page.</p>
+          <Link to="/">HOME</Link>
+        </div>
+        <div className="right-404"><img src={window.pix404URL}/></div>
+      </div>
+      <div className="bottom-404">
+        <img src={window.faintLogoURL}/>
+      </div>
+    </div>
+  );
+};
+
+export default Page404;
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../utils/route_util';
 import {Route, Switch} from 'react-router-dom';
-// import disguyText from '../../app/assets/images/disguy_logo.png';
 
 import Home from './home/home';
+import Page404 from './404page/four_o_four';
 
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/create_user_form_container';
@@ -19,6 +19,7 @@ const App = () => {
         <ProtectedRoute exact path="/placeholder" component={PlaceholderContainer} />
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        <Route component={Page404} />
       </Switch>
     </div>
   );
