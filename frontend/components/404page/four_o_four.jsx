8bc9b94ced64d9af45aab6dd91aebd07b77dbcf0
Chris Hanma <hongchris96@hotmail.com>
2021-04-07 11:00:13 -0700

    Css basic 404 page

--- frontend/components/404page/four_o_four.jsx
+++ frontend/components/404page/four_o_four.jsx
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom';
 
 const Page404 = (props) => {
   return (
-    <div className="page404">
+    <div className="page-404">
       <div className="body-404">
         <div className="left-404">
           <img src={window.title404URL}/>
-          <h2>WIZARDS BEHIND CURTAINS</h2>
+          <h2>WIZARDS BEHIND CURTAINS?</h2>
           <p>That is so 1939. disGuy is secretly powered by quantum robot bugeaters. 
             Anyway, you look lost here. No match found for <code>{props.location.pathname}</code>. 
             Here's the link back to Home page.</p>
