c85f93d0037dfa68ae734d08edfacc32f106a965
Chris Hanma <hongchris96@hotmail.com>
2021-04-11 20:46:01 -0700

    Complete models and associations for text channel, text channel messages, server members

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,10 +1,20 @@
 class Server < ApplicationRecord
 
   validates :server_name, presence: true, uniqueness: true
-  # validates :host_id, presence: true
 
   belongs_to :server_host,
     foreign_key: :host_id,
     class_name: :User
 
+  has_many :server_member_joins,
+    foreign_key: :server_id,
+    class_name: :ServerMember
+
+  has_many :members,
+    through: :server_member_joins,
+    source: :members
+
+  has_many :text_channels,
+    foreign_key: :server_id,
+    class_name: :TextChannel
 end
--- app/models/server_member.rb
+++ app/models/server_member.rb
@@ -1,2 +1,14 @@
 class ServerMember < ApplicationRecord
+  validates :server_id, :member_id, presence: true
+  validates :server_id, uniqueness: {scope: :member_id}
+
+  belongs_to :servers,
+    foreign_key: :server_id,
+    class_name: :Server
+
+  belongs_to :members,
+    foreign_key: :member_id,
+    class_name: :User
+
+
 end
