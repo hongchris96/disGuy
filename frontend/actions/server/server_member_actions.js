c45bbced484be3e829753c5b418912d2ea0d21d3
Chris Hanma <hongchris96@hotmail.com>
2021-05-05 13:58:16 -0700

    Add server member action

---
+++ frontend/actions/server/server_member_actions.js
@@ -0,0 +1,41 @@
+import * as ServerMemberAPIUtil from '../../utils/server_member_api_util';
+
+export const RECEIVE_SERVER_MEMBERS = "RECEIVE_SERVER_MEMBERS";
+export const JOIN_SERVER = "JOIN_SERVER";
+export const LEAVE_SERVER = "LEAVE_SERVER";
+
+export const RECEIVE_SERVER_MEMBER_ERRORS = "RECEIVE_SERVER_MEMBER_ERRORS";
+
+const receiveServerMembers = serverMembers => ({
+  type: RECEIVE_SERVER_MEMBERS,
+  serverMembers
+});
+
+const joinServer = serverMember => ({
+  type: JOIN_SERVER,
+  serverMember
+});
+
+const leaveServer = serverMemberId => ({
+  type: LEAVE_SERVER,
+  serverMemberId
+});
+
+const receiveServerMemberErrors = errors => ({
+  type: RECEIVE_SERVER_MEMBER_ERRORS,
+  errors
+});
+
+export const requestServerMembers = () => dispatch => {
+  return ServerMemberAPIUtil.fetchServerMembers().then(serverMembers => dispatch(receiveServerMembers(serverMembers)));
+};
+
+export const createServerMember = (serverMember) => dispatch => {
+  return ServerMemberAPIUtil.createServerMember(serverMember)
+    .then(serverMember => (dispatch(joinServer(serverMember))),
+    err => (dispatch(receiveServerMemberErrors(err.responseJSON))));
+};
+
+export const deleteServerMember = (serverMemberId) => dispatch => {
+  return ServerMemberAPIUtil.deleteServerMember(serverMemberId).then(() => dispatch(leaveServer(serverMemberId)));
+};
