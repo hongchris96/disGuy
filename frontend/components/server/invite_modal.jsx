3a147edea01ea7d4c61243af714cd39aeb7958d1
Chris Hanma <hongchris96@hotmail.com>
2021-05-07 10:34:20 -0700

    Add invite modal css and copy function

--- frontend/components/server/invite_modal.jsx
+++ frontend/components/server/invite_modal.jsx
@@ -6,24 +6,45 @@ class SeeInviteLink extends React.Component {
   constructor(props){
     super(props);
 
-    this.state = {invCode: this.props.currentServer.invite_code}
+    this.state = {
+      invCode: this.props.currentServer.invite_code,
+      cbutton: "Copy"
+    }
+
+    this.copy = this.copy.bind(this);
   }
 
   componentDidMount() {
     this.props.clearErrors();
   }
 
+  copy(e) {
+    document.querySelector('.inv-text').select();
+    document.execCommand('copy');
+    let copyButton = document.querySelector('.copy-button')
+    this.setState({cbutton: "Copied"});
+    copyButton.classList.add('copied');
+    setTimeout(() => {
+      copyButton.classList.remove('copied');
+      this.setState({cbutton: "Copy"})
+    }, 1000);
+  }
+
   render(){
 
     return(
       <div>
-        <form className="create-text-channel-form" onSubmit={this.handleSubmit}>
+        <form className="invite-link-modal">
           <p className="close-modal" onClick={() => this.props.closeModal()}>{`\u2715`}</p>
-          <div className="create-text-channel-heading">
+          <div className="invite-link-heading">
             <h1>INVITE FRIENDS TO SERVERNAME</h1>
           </div>
-          <label>SEND SERVER INVITE LINK TO A FRIEND</label>
-          <input type="text" value={this.state.invCode}/>
+          <div className="the-link">
+            <label>SEND SERVER INVITE LINK TO A FRIEND</label>
+            <input className="inv-text" type="text" value={this.state.invCode}/>
+            <button className="copy-button" onClick={this.copy}>{this.state.cbutton}</button>
+            <p>This is your invite link. Copy it and send it to your friend.</p>
+          </div>
         </form>
 
         <div className="blurred-background"></div>
