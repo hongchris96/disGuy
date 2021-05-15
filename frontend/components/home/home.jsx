d5ebfb53c1ec54c49aa18949706e05e05c0874ba
Chris Hanma <hongchris96@hotmail.com>
2021-05-14 17:54:23 -0700

    Splash page backboned and css

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -26,14 +26,62 @@ const Home = () => {
       
       <div className="home-div-2">
         <div>
-          <h1>{"¯\\_(ツ)_/¯"}</h1>
-        </div>
-        <div>
-          <h2>Some Title Here</h2>
-          <p>Some Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <img src={window.cogURL}/>
+          <div className="home-div-text">
+            <h2>Create an invite-only place where you belong</h2>
+            <p>disGuy servers are organized into topic-based 
+              channels where you can collaborate, share, and 
+              just talk about your day without clogging up a group chat.</p>
+          </div>
         </div>
       </div>
+
+      <div className="home-div-3">
+        <div>
+          <img src={window.cogURL}/>
+          <div className="home-div-text">
+            <h2>Where hanging out is easy</h2>
+            <p>Grab a seat in a voice channel when you’re free. 
+              Friends in your server can see you’re around and 
+              instantly pop in to talk without having to call.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="home-div-4">
+        <div>
+          <img src={window.cogURL}/>
+          <div className="home-div-text">
+            <h2>From few to a fandom</h2>
+            <p>Get any community running with moderation tools 
+              and custom member access. Give members special powers, 
+              set up private channels, and more.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="home-div-5">
+        <div>
+          <div className="home-div-text">
+            <h2>RELIABLE TECH FOR STAYING CLOSE</h2>
+            <p>Low-latency messaging feels like you’re in the same room. 
+              Type hello over chat, see friends talk about their days, or gather 
+              up and type hello to each other.</p>
+          </div>
+          <img src={window.cogURL}/>
+        </div>
+      </div>
+
+      <div className="home-div-footer">
+        <div>
+          <h2>Get to know me</h2>
+          <p>Portfolio</p>
+          <p>Github</p>
+          <p>LinkedIn</p>
+          <p>AngelList</p>
+        </div>
+      </div>
+
     </div>
   );
 };
