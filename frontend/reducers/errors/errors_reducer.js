b3c25f2e08dcf86788959aa593e39765aeb381d1
Chris Hanma <hongchris96@hotmail.com>
2021-05-16 20:46:06 -0700

    Add join server error handling

--- frontend/reducers/errors/errors_reducer.js
+++ frontend/reducers/errors/errors_reducer.js
@@ -4,13 +4,15 @@ import ServerErrorsReducer from './server_errors_reducer';
 import TextChannelErrorsReducer from './text_channel_errors_reducer';
 import TextChannelMessageErrorsReducer from './text_channel_message_errors_reducer';
 import DMChannelErrorsReducer from './dm_channel_errors_reducer';
+import JoinServerErrorsReducer from './join_server_errors_reducer';
 
 const ErrorReducer = combineReducers({
   session: SessionErrorsReducer,
   server: ServerErrorsReducer,
   textChannel: TextChannelErrorsReducer,
   textChannelMessage: TextChannelMessageErrorsReducer,
-  dmChannel: DMChannelErrorsReducer
+  dmChannel: DMChannelErrorsReducer,
+  joinServer: JoinServerErrorsReducer
 });
 
 export default ErrorReducer;
